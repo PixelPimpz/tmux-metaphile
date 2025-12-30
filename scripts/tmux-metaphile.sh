f003d082d4bdf1ba2f6a08fdba27a50ede8b8517
lucifer-morningstar <slobeck@gmail.com>
2025-12-30 09:48:06 -0800

    Generic commit

--- scripts/tmux-metaphile.sh
+++ scripts/tmux-metaphile.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-tmux display -p "tmux-metaphile.sh running..."
+#tmux display -p "tmux-metaphile.sh running..."
+tmux run-shell '$TMUX_ROOT/plugins/tmux-clockette/scripts/tmux-clockette.sh'
